e95a7b6f9bb08dbdac68aa00838d2c7ed1890f33
Aroob <aroob330@gmail.com>
2022-03-17 12:31:51 +0100

    metabox - productos

--- inc/productos-cpt.php
+++ inc/productos-cpt.php
@@ -93,4 +93,40 @@ function productos_cpt() {
 }
 add_action( 'init', 'productos_cpt', 0 );
 
+/*
+function wporg_add_custom_box() {
+	$screens = [ 'post', 'wporg_cpt' ];
+	foreach ( $screens as $screen ) {
+			add_meta_box(
+					'wporg_box_id',                 // Unique ID
+					'Custom Meta Box Title',      // Box title
+					'wporg_custom_box_html',  // Content callback, must be of type callable
+					$screen                            // Post type
+			);
+	}
+}
+add_action( 'add_meta_boxes', 'wporg_add_custom_box' );
+
+function wporg_custom_box_html( $post ) {
+    ?>
+    <label for="wporg_field">Description for this field</label>
+    <select name="wporg_field" id="wporg_field" class="postbox">
+        <option value="">Select something...</option>
+        <option value="something">Something</option>
+        <option value="else">Else</option>
+    </select>
+    <?php
+}
+
+function wporg_save_postdata( $post_id ) {
+    if ( array_key_exists( 'wporg_field', $_POST ) ) {
+        update_post_meta(
+            $post_id,
+            '_wporg_meta_key',
+            $_POST['wporg_field']
+        );
+    }
+}
+add_action( 'save_post', 'wporg_save_postdata' );
+*/
 ?>
